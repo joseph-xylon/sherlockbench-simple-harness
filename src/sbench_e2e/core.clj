050e72942aba58c676bc862352b30f668fe516b8
Joseph Graham <joseph@xylon.me.uk>
2026-05-07 06:56:58 +0100

    prove chat completion

--- src/sbench_e2e/core.clj
+++ src/sbench_e2e/core.clj
@@ -7,9 +7,23 @@
   (edn/read-string
    (slurp file-path)))
 
+(defn call-qwen
+  [config prompt]
+  (api/create-chat-completion
+    {:model "qwen3"
+     :messages [{:role "user" :content prompt}]}
+
+    {:api-endpoint (:llm-api config)}))
+
 (defn -main [problem-set & args]
   (let [config (read-edn-file "resources/config.edn")]
     (prn (str "problem-set: " problem-set))
     (prn (:server-url config))
     )
+  )
+
+(comment
+  (def config (read-edn-file "resources/config.edn"))
+
+  (call-qwen config "Would you love a monsterman?")
   )
