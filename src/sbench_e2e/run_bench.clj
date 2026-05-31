84529d0ddba99bab4d61963348dd24d3b31e5b83
Joseph Graham <joseph@xylon.me.uk>
2026-05-31 17:38:45 +0100

    fix command-line bug

--- src/sbench_e2e/run_bench.clj
+++ src/sbench_e2e/run_bench.clj
@@ -4,7 +4,7 @@
             [cheshire.core :as json]))
 
 (defn start-run [postfn problem-set attempts-per-problem]
-  (let [post-data (conj (utils/map-of problem-set attempts-per-problem)
+  (let [post-data (conj (utils/map-of problem-set (Integer/parseInt attempts-per-problem))
                         {:client-id "sbench_e2e"})
         {:keys [run-id attempts]} (postfn nil "start-run" post-data)
         postfn' (partial postfn run-id)]
